778095e4ca81a3decc01dc5c12c54f01533170bb
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2025-02-16 17:59:36 -0600

    elementos para el analisis lexico prueba

---
+++ ErrorLexico.js
@@ -0,0 +1,6 @@
+class ErrorLexico {
+    constructor(simbolo, posicion) {
+        this.simbolo = simbolo;
+        this.posicion = posicion;
+    }
+}
---
+++ Token.js
@@ -0,0 +1,7 @@
+class Token {
+    constructor(tipo, valor, posicion) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.posicion = posicion;
+    }
+}
